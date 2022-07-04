87b46a24e1e08e701c7e308ee894216d319c2b2a
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-07-04 07:01:56 -0300

    update database

--- src/config/database.js
+++ src/config/database.js
@@ -3,10 +3,10 @@ require('dotenv/config');
 module.exports = {
   dialect: 'postgres',
   port: 5432,
-  host: 'ec2-44-206-89-185.compute-1.amazonaws.com',
-  username:  'kiduwwtzbcuglg',
-  password: '2cf612617cae0ceda5bea73b2166a2784eb6d236da4b05d40667718af9bd2e36',
-  database: 'd11c5a9lak8ch6',
+  host: 'jelani.db.elephantsql.com',
+  username: 'tguzwtkh',
+  password: 'AOpGUOz4YfJvCaCSNE7EjEs08qzBaftG',
+  database: 'tguzwtkh',
   define: {
     timeStamps: true,
     underscored: true,
