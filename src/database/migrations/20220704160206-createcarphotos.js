c710914453b50b00b0ea2677dc9b7452e2018a61
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-07-04 17:18:09 -0300

    update features

--- src/database/migrations/20220704160206-createcarphotos.js
+++ src/database/migrations/20220704160206-createcarphotos.js
@@ -2,12 +2,43 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    queryInterface.createTable('createcarphotos', {
-      // table
+    return queryInterface.createTable('createcarphotos', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      adverts_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'adverts', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      photo_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'carphotos', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      }
     })
   },
 
   down: queryInterface => {
-    queryInterface.dropTable('createcarphotos');
+    return queryInterface.dropTable('createcarphotos');
   }
 };
--- src/database/migrations/20220704161534-createcaradditionals.js
+++ src/database/migrations/20220704161534-createcaradditionals.js
@@ -3,7 +3,34 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('createcaradditionals', {
-      // table content
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      adverts_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'adverts', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      additional_id: {
+        type: Sequelize.INTEGER,
+        references: { model: 'additionals', key: 'id'},
+        onUpdate: 'CASCADE',
+        onDelete: 'SET NULL',
+        allowNull: true
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
     })
   },
 
