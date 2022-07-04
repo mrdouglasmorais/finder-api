f32c3225bc997d504c5302b346bc29c5edc23e2d
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-07-04 17:46:32 -0300

    update relations

--- src/database/migrations/20220704160206-createcarphotos.js
+++ src/database/migrations/20220704160206-createcarphotos.js
@@ -1,44 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('createcarphotos', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      url: {
-        type: Sequelize.STRING,
-        allowNull: false,
-      },
-      adverts_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'adverts', key: 'id'},
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true
-      },
-      photo_id: {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'adverts',
+      'photo_id',
+      {
         type: Sequelize.INTEGER,
         references: { model: 'carphotos', key: 'id'},
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
         allowNull: true
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
       }
-    })
+    )
   },
 
-  down: queryInterface => {
-    return queryInterface.dropTable('createcarphotos');
+  down: async queryInterface => {
+    return queryInterface.removeColumn('adverts', 'photo_id');
   }
 };
--- src/database/migrations/20220704161534-createcaradditionals.js
+++ src/database/migrations/20220704161534-createcaradditionals.js
@@ -1,40 +1,21 @@
 'use strict';
 
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('createcaradditionals', {
-      id: {
-        type: Sequelize.INTEGER,
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true
-      },
-      adverts_id: {
-        type: Sequelize.INTEGER,
-        references: { model: 'adverts', key: 'id'},
-        onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
-        allowNull: true
-      },
-      additional_id: {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.addColumn(
+      'adverts',
+      'additional_id',
+      {
         type: Sequelize.INTEGER,
         references: { model: 'additionals', key: 'id'},
         onUpdate: 'CASCADE',
         onDelete: 'SET NULL',
         allowNull: true
-      },
-      created_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-      updated_at: {
-        type: Sequelize.DATE,
-        allowNull: false,
-      },
-    })
+      }
+    )
   },
 
-  down: queryInterface => {
-    return queryInterface.dropTable('createcaradditionals')
+  down: async queryInterface => {
+    return queryInterface.removeColumn('adverts', 'additional_id');
   }
 };
