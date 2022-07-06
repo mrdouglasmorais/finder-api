1fffd40b0825ac92169d159fea20100785c1d150
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-07-06 11:51:41 -0300

    remove photos update route

--- src/routes.js
+++ src/routes.js
@@ -11,7 +11,7 @@ import CarModelController from './app/controllers/CarModelControllers';
 import MileageController from './app/controllers/MileageController';
 import FuelController from './app/controllers/FuelController';
 import AdditionalController from './app/controllers/AdditionalController';
-import CarPhotosController from './app/controllers/CarPhotosController';
+// import CarPhotosController from './app/controllers/CarPhotosController';
 import AdvertsController from './app/controllers/AdvertsController';
 
 // swagger
@@ -69,9 +69,9 @@ routes.get('/fuel', FuelController.index);
 routes.post('/additional', AdditionalController.store);
 routes.get('/additional', AdditionalController.index);
 
-// Adverts
-routes.post('/photos', CarPhotosController.store);
-routes.get('/photos', CarPhotosController.index);
+// Carphotos
+// routes.post('/photos', CarPhotosController.store);
+// routes.get('/photos', CarPhotosController.index);
 
 // Adverts
 routes.post('/adverts', AdvertsController.store);
