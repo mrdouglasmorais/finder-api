642a894e6905769946e0b5122f2ee64c1bb53231
douglas.morais@i-value.com.br <douglas.morais@i-value.com.br>
2022-07-05 22:28:05 -0300

    update results for

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ require('dotenv/config');
 
 module.exports = {
   dialect: 'postgres',
-  port: 5432,
-  host: 'jelani.db.elephantsql.com',
-  username: 'tguzwtkh',
-  password: 'AOpGUOz4YfJvCaCSNE7EjEs08qzBaftG',
-  database: 'tguzwtkh',
+  port: 25060,
+  host: 'app-a654fa7a-2311-458e-ab61-7bf64f091dd9-do-user-11941045-0.b.db.ondigitalocean.com',
+  username: 'finderdb',
+  password: 'AVNS_GS0CJ4_atuTO0TTBP9N',
+  database: 'finderdb',
   define: {
     timeStamps: true,
     underscored: true,
